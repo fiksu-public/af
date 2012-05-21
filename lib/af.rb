ed3f928a289cc7eafd276e9d68677b076b33a5fa
Keith Gabryelski <keith@fiksu.com>
2012-05-21 13:43:42 -0400

    DaemonProcess renamed to Application

--- lib/af.rb
+++ lib/af.rb
@@ -1,7 +1,7 @@
 require 'af/advisory_locker'
 require 'af/get_options'
 require 'af/command_line_tool_mixin'
-require 'af/daemon_process'
+require 'af/application'
 require 'af/q_thread/interface'
 require 'af/q_thread/base'
 require 'af/q_thread/message'
