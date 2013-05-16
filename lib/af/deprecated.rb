c2d15d2f7ae860aae5964987c34a95934c7dcf32
Keith Gabryelski <keith@gabryelski.com>
2013-05-15 21:15:36 -0400

    pull out Af Module

--- lib/af/deprecated.rb
+++ lib/af/deprecated.rb
@@ -1,13 +1,15 @@
-module Af::Deprecated
-  def self.included(base)
-    base.extend(ClassMethods)
-  end
+module Af
+  module Deprecated
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
 
-  def update_opts(*x)
-    puts "don't use update_opts -- use #opt (#{caller.first})"
-    self.class.opt *x
-  end
+    def update_opts(*x)
+      puts "don't use update_opts -- use #opt (#{caller.first})"
+      self.class.opt *x
+    end
 
-  module ClassMethods
+    module ClassMethods
+    end
   end
 end
